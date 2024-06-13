a27caf6084b4ecdc9317484fed96225b453ca642
Dmitrii Bulatov <bulati484@gmail.com>
2024-06-13 17:54:32 +0300

    some corrections

--- vite.config.js
+++ vite.config.js
@@ -10,38 +10,56 @@ export default defineConfig({
       registerType: 'autoUpdate',
       outDir: 'dist',
       manifest: {
-      name: 'PhoheBook',
-      short_name: 'BOOK',
-      description: 'PhoneBook of my collegues',
-      theme_color: '#ffffff',
-      start_url: "/",
-      display: "standalone",
-      background_color: "#ffffff",
-      icons: [
-        {
-          src: "/pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "any"
-        },
-        {
-          src: "/pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any"
-        },
-        {
-          src: "/pwa-maskable-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "maskable"
-        },
-        {
-          src: "/pwa-maskable-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable"
-        }],
+        name: 'PhoheBook',
+        short_name: 'BOOK',
+        description: 'PhoneBook of my collegues',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: "assets/icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "assets/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "assets/icons/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "assets/icons/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+
+          {
+            src: "assets/icons/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "assets/icons/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "assets/icons/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+            purpose: "maskable any"
+          }
+
+        ],
       }
     })
   ],
